31a39026afbb72781db8a12124d8cfcc43b72d0a
nundung <147980144+nundung@users.noreply.github.com>
2024-02-25 16:22:16 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240225/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout<<"Total days in Year"<<endl<<"365"<<endl<<"Circumference rate"<<endl<<"3.1415926535";
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
