fa1792fc297e740f8516b6386825df755c3f719b
nundung <147980144+nundung@users.noreply.github.com>
2024-02-27 22:16:51 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240227/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 13;
+    double b = 0.165;
+    cout << fixed;
+    cout.precision(6);
+    cout << a << " * " << b << " = " << a*b;
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
