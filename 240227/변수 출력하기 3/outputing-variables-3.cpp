eca3231d41520b39e04b7c87baf825f955bfe5da
nundung <147980144+nundung@users.noreply.github.com>
2024-02-27 22:09:27 +0900

    Title: 변수 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240227/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 1;
+    int b = 2;
+    string c = "C";
+    cout << a << "->" << b << "->" << c;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
