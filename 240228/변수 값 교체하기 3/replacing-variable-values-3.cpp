baa541afe2c2847a0d07649879812cf59a8fb211
nundung <147980144+nundung@users.noreply.github.com>
2024-02-28 15:12:29 +0900

    Title: 변수 값 교체하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240228/변수 값 교체하기 3/replacing-variable-values-3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 3, b = 5;
+    int x;
+
+    x = a;
+    a = b;
+    b = x;
+
+    cout << a << endl << 3; 
+    return 0;
+}
