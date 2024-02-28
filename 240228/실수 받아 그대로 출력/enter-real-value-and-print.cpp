af22d3562d6ccbeacbd6816f29a792b08818ee71
nundung <147980144+nundung@users.noreply.github.com>
2024-02-28 16:07:33 +0900

    Title: 실수 받아 그대로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240228/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double n;
+    cin >> n;
+    cout << fixed;
+    cout.precision(2);
+    cout << n;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
