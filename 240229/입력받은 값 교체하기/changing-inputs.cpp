752acd566b282e33b5cb0d005c0eb85e45873f27
nundung <147980144+nundung@users.noreply.github.com>
2024-02-29 17:34:19 +0900

    Title: 입력받은 값 교체하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240229/입력받은 값 교체하기/changing-inputs.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int x = a;
+    int y= b;
+    a = y;
+    b = x;
+
+
+    cout << a << " " << b;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
