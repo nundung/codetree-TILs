c7721d30b48bfd29f3dfcc29d73d132242bbd73d
nundung <147980144+nundung@users.noreply.github.com>
2024-02-29 20:19:30 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240229/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c;
+    char d, e;
+    cin >> a >> d >> b >> e >> c;
+    cout << c << "." << a << "." << b;
+    return 0;
+}
