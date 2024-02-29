d5c37caa27fd5a535f494245ee926093f1f2dd1f
nundung <147980144+nundung@users.noreply.github.com>
2024-02-29 18:47:06 +0900

    Title: 문자열 순서 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240229/문자열 순서 바꾸기/change-order-of-strings.cpp
+++ 240229/문자열 순서 바꾸기/change-order-of-strings.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     string s, t;
     cin >> s >> t;
     cout << t << endl << s;
