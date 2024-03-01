3a295ced89eaa78439daede7c66645a15910145b
nundung <147980144+nundung@users.noreply.github.com>
2024-03-01 19:19:35 +0900

    Title: 날짜 변경하여 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240301/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c;
+    char d, e;
+    cin >> a >> d >> b >> e >> c;
+    cout << b << "-" << c << "-" << a;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
