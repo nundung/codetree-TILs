abbed799024623e2b593ce99dfda95e3bb9ac574
nundung <147980144+nundung@users.noreply.github.com>
2024-03-01 19:23:19 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240301/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c;
+    char d, e;
+    cin >> a >> d >> b >> e >> c;
+    cout << "0" << a << "-" << c << "-" << b;
+    
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
