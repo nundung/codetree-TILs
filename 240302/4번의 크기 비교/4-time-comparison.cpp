6ea74222ee86687ba97cb09cd0f67406e3470cd1
nundung <147980144+nundung@users.noreply.github.com>
2024-03-02 19:03:07 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240302/4번의 크기 비교/4-time-comparison.cpp
+++ 240302/4번의 크기 비교/4-time-comparison.cpp
@@ -2,8 +2,7 @@
 using namespace std;
 int main() {
     int a, b, c, d, e;
-    cin >> a;
-    cin >> b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
     cout << (a > b) << '\n' << (a > c) << '\n' << (a > d) << '\n' << (a > e);
     // 여기에 코드를 작성해주세요.
     return 0;
