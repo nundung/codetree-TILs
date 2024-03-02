ee739305b6179963e243d9b87d761011528d9216
nundung <147980144+nundung@users.noreply.github.com>
2024-03-02 17:51:35 +0900

    Title: 4가지 관계연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240302/4가지 관계연산자/four-relational-operators.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << ( a >= b ) << '\n' << ( a > b ) << '\n' << ( b >= a ) << '\n' << ( b > a ); 
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
