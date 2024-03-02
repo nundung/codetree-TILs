65fa6ac7179d0d3665606ea36dc5e5b8539e84d0
nundung <147980144+nundung@users.noreply.github.com>
2024-03-02 17:01:32 +0900

    Title: 3 또는 5의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240302/3 또는 5의 배수/multiples-of-3-or-5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+    if ( a % 3 == 0 ) cout << "YES";
+    else cout << "NO";
+    cout << '\n';
+    if ( a % 5 == 0 ) cout << "YES";
+    else cout << "NO";
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
