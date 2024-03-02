e13c334e55773dbb98ab6fe2b4d0d6aaaab2389d
nundung <147980144+nundung@users.noreply.github.com>
2024-03-02 16:50:43 +0900

    Title: 살 수 있는 물건 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240302/살 수 있는 물건 2/things-able-to-buy-2.cpp
+++ 240302/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -3,17 +3,10 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    if (n >= 3000) {
-        cout << "book";
-    }
-    else if (n >= 1000) {
-        cout << "mask";
-    }
-    else if (n >= 500) {
-        cout << "pen";
-    }
-    else {
-        cout << "no";
-    }
+
+    if (n >= 3000) cout << "book";
+    else if (n >= 1000) cout << "mask";
+    else if (n >= 500) cout << "pen";
+    else cout << "no";
     return 0;
 }
