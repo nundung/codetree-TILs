43aef29990b2617fa5b42aefc5f3cad30b2bd079
nundung <147980144+nundung@users.noreply.github.com>
2024-03-03 19:04:29 +0900

    Title: 세 정수의 최솟값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240303/세 정수의 최솟값/minimum-of-three-numbers.cpp
+++ 240303/세 정수의 최솟값/minimum-of-three-numbers.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main() {
     int a, b, c;
     cin >> a >> b >> c;
-    if ( a < b && a < c ) cout << a;
-    else if ( b < a && b < c ) cout << b;
+    if ( a <= b && a <= c ) cout << a;
+    else if ( b <= a && b <= c ) cout << b;
     else cout << c; 
     // 여기에 코드를 작성해주세요.
     return 0;
