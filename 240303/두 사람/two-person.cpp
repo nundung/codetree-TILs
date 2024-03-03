a81cee47cbfde266d8b2ef1099e336fe14e59cd9
nundung <147980144+nundung@users.noreply.github.com>
2024-03-03 19:23:02 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240303/두 사람/two-person.cpp
+++ 240303/두 사람/two-person.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main() {
     int a, c;
-    char b, d;
+    string b, d;
     cin >> a >> b >> c >> d;
-    cout << ( a >= 19 && b == "M" || c >= 19 && d == "M" );
+    cout << ( (a >= 19 && b == "M") || (c >= 19 && d == "M") );
     // 여기에 코드를 작성해주세요.
     return 0;
 }
