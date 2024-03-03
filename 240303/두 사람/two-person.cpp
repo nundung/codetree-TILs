f9272556c10ba4417c6427b3a9db64990e613fff
nundung <147980144+nundung@users.noreply.github.com>
2024-03-03 19:16:58 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240303/두 사람/two-person.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, c;
+    char b, d;
+    cin >> a >> b >> c >> d;
+    cout << ( a >= 19 && b == "M" || c >= 19 && d == "M" );
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
