cfba4a29b6f0db70c1b5d9565f338d370837d037
Tae Eun Jeong <147980144+nundung@users.noreply.github.com>
2024-03-06 16:33:41 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240306/윤년인가/is-leap-year.cpp
+++ 240306/윤년인가/is-leap-year.cpp
@@ -3,10 +3,8 @@ using namespace std;
 int main() {
     int a;
     cin >> a;
-    if ( a % 4 == 0 ) {
-        if ( a % 100 == 0 && a % 400 != 0) cout << "false";
-        else cout << "true";
-    } else cout << "false";
+    if ( a % 4 == 0 && a % 100 != 0 || a % 400 == 0) cout << "true";
+    else cout << "false";
     // 여기에 코드를 작성해주세요.
     return 0;
 }
