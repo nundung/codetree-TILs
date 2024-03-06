2bcf18aa1bb066a6f8f43cc979455baff1e03372
Tae Eun Jeong <147980144+nundung@users.noreply.github.com>
2024-03-06 16:45:43 +0900

    Title: 일 수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240306/일 수 구하기/number-of-days-in-month.cpp
+++ 240306/일 수 구하기/number-of-days-in-month.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main() {
     int a;
     cin >> a;
-    if ( a % 2 == 1 || a == 8 ) cout << 31;
+    if ( a  == 4 || a == 6 || a == 9 || a == 11) cout << 30;
     else if ( a == 2 ) cout << 28;
-    else cout << 30;
+    else cout << 31;
     // 여기에 코드를 작성해주세요.
     return 0;
 }
