9419909608f86fcf9e680427b9b2b722311a618d
Tae Eun Jeong <147980144+nundung@users.noreply.github.com>
2024-03-09 16:23:08 +0900

    Title: 홀수만 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240309/홀수만 출력 2/output-only-odd-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cin >> b >> a;
+    for (int i=b; i>a-1; i--) {
+        if (i%2==1) cout << i << " ";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
