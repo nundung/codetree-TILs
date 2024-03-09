8cc62c2508851e0ab6301a03d7a6c05ebc861bf4
Tae Eun Jeong <147980144+nundung@users.noreply.github.com>
2024-03-09 16:29:44 +0900

    Title: 별 n개 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240309/별 n개 출력하기/print-n-stars.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    int i=1;
+    cin >> a;
+    while (i<a+1) {
+        cout<<"*"<<'\n';
+        i++;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
