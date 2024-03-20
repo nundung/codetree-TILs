51b7f9f1323157906d720279c984f4b57e194d19
Tae Eun Jeong <147980144+nundung@users.noreply.github.com>
2024-03-20 19:02:02 +0900

    Title: 1까지 나누기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240320/1까지 나누기/divide-by-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double n;
+    cin >> n;
+    for (int i=1; i<100; i++) {
+        n /= i;
+        if(n<=1) {
+            cout << i;
+            break;
+        }
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
