9a94affcf45e220803537abb5116e2a833a6ad26
Tae Eun Jeong <147980144+nundung@users.noreply.github.com>
2024-03-21 16:57:40 +0900

    Title: 합성수 판별기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240321/합성수 판별기/factorization-discriminator.cpp
+++ 240321/합성수 판별기/factorization-discriminator.cpp
@@ -4,8 +4,8 @@ int main() {
     int n;
     cin >> n;
     bool isTrue = false;
-    for (int i=1; i<n; i++) {
-        if (n%i==0) {
+    for (int i=2; i<n; i++) {
+        if (n%i!=0) {
             isTrue = true;
             break;
         }
