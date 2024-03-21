75e33d518b61b230ee6d9371eda6d5586194aa99
Tae Eun Jeong <147980144+nundung@users.noreply.github.com>
2024-03-21 17:02:30 +0900

    Title: 합성수 판별기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240321/합성수 판별기/factorization-discriminator.cpp
+++ 240321/합성수 판별기/factorization-discriminator.cpp
@@ -3,14 +3,14 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    bool isTrue = false;
+    bool isC = false;
     for (int i=2; i<n; i++) {
-        if (n%i!=0) {
-            isTrue = true;
+        if (n%i==0) {
+            isC = true;
             break;
         }
     }
-    if (isTrue) cout << "C";
+    if (isC) cout << "C";
     else cout << "N";
     // 여기에 코드를 작성해주세요.
     return 0;
