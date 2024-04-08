048052710581834c6eea649ddd26a4436121b97e
Tae Eun Jeong <147980144+nundung@users.noreply.github.com>
2024-04-08 19:07:07 +0900

    Title: 별표 출력하기 5, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240408/별표 출력하기 5/print-star-5.cpp
+++ 240408/별표 출력하기 5/print-star-5.cpp
@@ -3,49 +3,58 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    
-    for (int i=n; i>0; i--) {
-        for (int j=0; j<n; j++) {
-           cout << "*";
+    for (int l=n; l>0; l--) {
+        for (int i=n; i>0; i--) {
+            for (int j=0; j<n; j++) {
+            cout << "*";
+            }
+            cout << " ";
         }
-        cout << " ";
+        cout << endl;
+        n -=1;
     }
-    cout << endl;
-    n -=1;
+    // for (int i=n; i>0; i--) {
+    //     for (int j=0; j<n; j++) {
+    //        cout << "*";
+    //     }
+    //     cout << " ";
+    // }
+    // cout << endl;
+    // n -=1;
 
-    for (int i=n; i>0; i--) {
-        for (int j=0; j<n; j++) {
-           cout << "*";
-        }
-        cout << " ";
-    }
-    cout << endl;
-    n -=1;
+    // for (int i=n; i>0; i--) {
+    //     for (int j=0; j<n; j++) {
+    //        cout << "*";
+    //     }
+    //     cout << " ";
+    // }
+    // cout << endl;
+    // n -=1;
 
-    for (int i=n; i>0; i--) {
-        for (int j=0; j<n; j++) {
-           cout << "*";
-        }
-        cout << " ";
-    }
-    cout << endl;
-    n -=1;
+    // for (int i=n; i>0; i--) {
+    //     for (int j=0; j<n; j++) {
+    //        cout << "*";
+    //     }
+    //     cout << " ";
+    // }
+    // cout << endl;
+    // n -=1;
     
-    for (int i=n; i>0; i--) {
-        for (int j=0; j<n; j++) {
-           cout << "*";
-        }
-        cout << " ";
-    }
-    cout << endl;
-    n -=1;
-    
-    for (int i=n; i>0; i--) {
-        for (int j=0; j<n; j++) {
-           cout << "*";
-        }
-        cout << " ";
-    }
+    // for (int i=n; i>0; i--) {
+    //     for (int j=0; j<n; j++) {
+    //        cout << "*";
+    //     }
+    //     cout << " ";
+    // }
+    // cout << endl;
+    // n -=1;
+
+    // for (int i=n; i>0; i--) {
+    //     for (int j=0; j<n; j++) {
+    //        cout << "*";
+    //     }
+    //     cout << " ";
+    // }
     // 여기에 코드를 작성해주세요.
     return 0;
 }
