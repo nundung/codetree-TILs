813ceae023fd710d30a722718f74b01808873eda
Tae Eun Jeong <147980144+nundung@users.noreply.github.com>
2024-04-08 17:29:08 +0900

    Title: 별표 출력하기 5, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240408/별표 출력하기 5/print-star-5.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for (int i=n; i>0; i--) {
+        for (int j=0; j<n; j++) {
+           cout << "*";
+        }
+        cout << " ";
+    }
+    cout << endl;
+    n -=1;
+    for (int i=n; i>0; i--) {
+        for (int j=0; j<n; j++) {
+           cout << "*";
+        }
+        cout << " ";
+    }
+    cout << endl;
+    n -=1;
+
+    for (int i=n; i>0; i--) {
+        for (int j=0; j<n; j++) {
+           cout << "*";
+        }
+        cout << " ";
+    }
+    cout << endl;
+    n -=1;
+    
+    for (int i=n; i>0; i--) {
+        for (int j=0; j<n; j++) {
+           cout << "*";
+        }
+        cout << " ";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
